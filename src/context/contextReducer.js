70c2c293169920c59027d9aed0a8506c547f6018
hanhngooo <hanhngo1795@gmail.com>
2021-05-09 21:38:24 +0200

    create useReducer

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,3 +1,16 @@
-const contextReducer = (state, action) => {};
+const contextReducer = (state, action) => {
+  let transactions;
+  switch (action.type) {
+    case "DELETE_TRANCTION":
+      transactions = state.filter((t) => t.id !== action.payload);
+      return transactions;
+
+    case "ADD_TRANCTION":
+      transactions = [action.payload, ...state];
+      return transactions;
+    default:
+      break;
+  }
+};
 
 export default contextReducer;
