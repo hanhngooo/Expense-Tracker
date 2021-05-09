87d40f8edec57d1bb97ff597863b3b74677b3808
hanhngooo <hanhngo1795@gmail.com>
2021-05-09 23:24:16 +0200

    create custom hook to use transaction data for charts

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,15 +2,17 @@ import React from "react";
 import { Card, CardHeader, CardContent, Typography } from "@material-ui/core";
 import { Doughnut } from "react-chartjs-2";
 import useStyles from "./style";
+import useTransactions from "../../useTransactions";
 
 const Details = ({ detailClass, title }) => {
   const classes = useStyles();
+  const { total, chartData } = useTransactions(title);
   return (
     <Card className={classes[`${detailClass}`]}>
       <CardHeader title={title} />
       <CardContent>
-        <Typography variant="h5">50</Typography>
-        {/* <Doughnut data="DATA" /> */}
+        <Typography variant="h5">${total}</Typography>
+        <Doughnut data={chartData} />
       </CardContent>
     </Card>
   );
