c557eabb6697f283809b2b9eefb4b68981177442
hanhngooo <hanhngo1795@gmail.com>
2021-05-10 02:58:23 +0200

    minor change

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -3,7 +3,7 @@ const isIncome = Math.round(Math.random());
 const InfoCard = () => {
   return (
     <div style={{ textAlign: "center", padding: "0 10%" }}>
-      Try saying: <br />
+      Press and hold.Try saying: <br />
       Add {isIncome ? "Income " : "Expense "}
       for {isIncome ? "$100 " : "$50 "}
       into category {isIncome ? "Salary " : "Food "}
